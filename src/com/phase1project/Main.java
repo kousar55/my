0747d1498726354cba02d54224361002512f7e37
kousar55 <jamadarkousar55@gmail.com>
2022-08-16 18:43:00 +0530

    Initial commit

---
+++ src/com/phase1project/Main.java
@@ -0,0 +1,14 @@
+package com.phase1project;
+
+public class Main {
+    /*Enter your desired Directory path */
+    public static final String path = "C:\\Users\\kousarj\\OneDrive - AMDOCS\\Backup Folders\\Desktop\\JDBC\\git_JDBC\\FSDPhase-1-Virtual-key-for-repository-";
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        FileOPList menu = new FileOPList();
+        menu.introScreen();
+        menu.mainMenu();
+
+    }
+
+}
